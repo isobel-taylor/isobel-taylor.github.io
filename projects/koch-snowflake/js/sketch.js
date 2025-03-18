9d75d78e65eb32305302c6dccc9595cf79168c34
Isobel Taylor-Joyce <offtheball@protonmail.com>
2025-03-18 04:02:10 +0000

    Rainbow snowflake

--- projects/koch-snowflake/js/sketch.js
+++ projects/koch-snowflake/js/sketch.js
@@ -2,17 +2,21 @@ function setup() {
     createCanvas(600, 600);
 
     angleMode(DEGREES);
+    colorMode(HSB, 360);
 
     // Calculate the points around a circle at three equal spaces, and make our
     // snowflake out of them.
     window.snowflake = [0, 1, 2].map((index) => {
-        const r = width / 2.2;
+        const r = min(width, height) / 2.2;
         const x1 = r * cos(index * 120 + 90);
         const y1 = r * sin(index * 120 + 90);
         const x2 = r * cos((index + 1) * 120 + 90);
         const y2 = r * sin((index + 1) * 120 + 90);
 
-        return new Snowflake(x1, y1, x2, y2);
+        const colourStart = index * 120;
+        const colourEnd = (index + 1) * 120;
+
+        return new Snowflake(x1, y1, x2, y2, colourStart, colourEnd);
     });
 }
 
--- projects/koch-snowflake/js/snowflake.js
+++ projects/koch-snowflake/js/snowflake.js
@@ -1,7 +1,10 @@
 class Snowflake {
-    constructor(x1, y1, x2, y2) {
+    constructor(x1, y1, x2, y2, colourStart, colourEnd) {
         this.p1 = createVector(x1, y1);
         this.p2 = createVector(x2, y2);
+
+        this.colourStart = colourStart;
+        this.colourEnd = colourEnd;
 
         this.children = [];
     }
@@ -16,17 +19,64 @@ class Snowflake {
         if (this.children.length === 0) {
             // Calculate the three key points to which the child segments will
             // attach.
-            const one_third = p5.Vector.lerp(this.p1, this.p2, 1 / 3);
-            const two_third = p5.Vector.lerp(this.p1, this.p2, 2 / 3);
-            const point = p5.Vector.sub(one_third, two_third)
-                .rotate(60)
-                .add(two_third);
+            const oneQuarterPoint = p5.Vector.lerp(this.p1, this.p2, 1 / 3);
+            const threeQuarterPoint = p5.Vector.lerp(this.p1, this.p2, 2 / 3);
+            const twoQuarterPoint = p5.Vector.sub(
+                threeQuarterPoint,
+                oneQuarterPoint,
+            )
+                .rotate(-60)
+                .add(oneQuarterPoint);
+
+            const oneQuarterColour = lerp(
+                this.colourStart,
+                this.colourEnd,
+                1 / 4,
+            );
+            const twoQuarterColour = lerp(
+                this.colourStart,
+                this.colourEnd,
+                1 / 2,
+            );
+            const threeQuarterColor = lerp(
+                this.colourStart,
+                this.colourEnd,
+                3 / 4,
+            );
 
             this.children = [
-                new Snowflake(this.p1.x, this.p1.y, one_third.x, one_third.y),
-                new Snowflake(one_third.x, one_third.y, point.x, point.y),
-                new Snowflake(point.x, point.y, two_third.x, two_third.y),
-                new Snowflake(two_third.x, two_third.y, this.p2.x, this.p2.y),
+                new Snowflake(
+                    this.p1.x,
+                    this.p1.y,
+                    oneQuarterPoint.x,
+                    oneQuarterPoint.y,
+                    this.colourStart,
+                    oneQuarterColour,
+                ),
+                new Snowflake(
+                    oneQuarterPoint.x,
+                    oneQuarterPoint.y,
+                    twoQuarterPoint.x,
+                    twoQuarterPoint.y,
+                    oneQuarterColour,
+                    twoQuarterColour,
+                ),
+                new Snowflake(
+                    twoQuarterPoint.x,
+                    twoQuarterPoint.y,
+                    threeQuarterPoint.x,
+                    threeQuarterPoint.y,
+                    twoQuarterColour,
+                    threeQuarterColor,
+                ),
+                new Snowflake(
+                    threeQuarterPoint.x,
+                    threeQuarterPoint.y,
+                    this.p2.x,
+                    this.p2.y,
+                    threeQuarterColor,
+                    this.colourEndt,
+                ),
             ];
         } else {
             for (const children of this.children) {
@@ -37,9 +87,35 @@ class Snowflake {
 
     show() {
         if (this.children.length === 0) {
-            stroke(255);
-            strokeWeight(1);
-            line(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
+            const SEGMENTS = 12;
+
+            for (let i = 0; i < SEGMENTS; i++) {
+                const startPoint = p5.Vector.lerp(
+                    this.p1,
+                    this.p2,
+                    i / SEGMENTS,
+                );
+                const endPoint = p5.Vector.lerp(
+                    this.p1,
+                    this.p2,
+                    (i + 1) / SEGMENTS,
+                );
+
+                const startColour = lerp(
+                    this.colourStart,
+                    this.colourEnd,
+                    i / SEGMENTS,
+                );
+                const endColour = lerp(
+                    this.colourStart,
+                    this.colourEnd,
+                    (i + 1) / SEGMENTS,
+                );
+
+                strokeWeight(1);
+                stroke((startColour + endColour) / 2, 360, 360);
+                line(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+            }
         } else {
             for (const children of this.children) {
                 children.show();
